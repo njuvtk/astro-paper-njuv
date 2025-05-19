d1b4fa527461b776ce363dd0f463910f15a2c873
Huixiang Luo <njuvtk@outlook.com>
2025-05-19 10:09:33 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -17,6 +17,6 @@ export const SITE = {
     url: "https://github.com/satnaing/astro-paper/edit/main/",
   },
   dynamicOgImage: false,
-  lang: "en", // html lang code. Set this empty and default will be "en"
+  lang: "zh_CN", // html lang code. Set this empty and default will be "en"
   timezone: "Asia/Shanghai", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
